fd8f6f5085bc675306f16f84d42fd3fcf4c5e85b
Manikandan Sundararajan <mani@hey.com>
2023-05-30 03:56:04 +0000

    Build the catscii executable using crane

--- flake.nix
+++ flake.nix
@@ -9,8 +9,16 @@
         flake-utils.follows = "flake-utils";
       };
     };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        rust-overlay.follows = "rust-overlay";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
@@ -20,15 +28,45 @@
           };
           # this refers to the path ./rust-toolchain.toml
           rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          # tell crane to use our toolchain & use our private shipyard registry
+          craneLibWithoutRegistry = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+          shipyardToken = builtins.readFile ./secrets/shipyard-token;
+          craneLib = craneLibWithoutRegistry.appendCrateRegistries [
+            (craneLibWithoutRegistry.registryFromDownloadUrl {
+              dl = "https://crates.shipyard.rs/api/v1/crates";
+              indexUrl = "ssh://git@ssh.shipyard.rs/itsrainingmani/crate-index.git";
+              fetchurlExtraArgs = {
+                curlOptsList = [ "-H" "user-agent: shipyard ${shipyardToken}" ];
+              };
+            })
+          ];
+          src = craneLib.cleanCargoSource ./.;
           # things you only need at compile-time
           nativeBuildInputs = with pkgs; [ rustToolchain pkg-config sqlite ];
           # things you also need at run-time
           buildInputs = with pkgs; [ openssl ];
+          # this will be used for both `cargoArtifacts` and `bin`
+          commonArgs = {
+            inherit src buildInputs nativeBuildInputs;
+          };
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          # remember, `set1 // set2` does a shallow merge:
+          bin = craneLib.buildPackage (commonArgs // {
+            inherit cargoArtifacts;
+          });
         in
         with pkgs;
         {
+          packages =
+            {
+              # so we can build `bin` specifically
+              # but it's also the default
+              inherit bin;
+              default = bin;
+            };
           devShells.default = mkShell {
-            inherit buildInputs nativeBuildInputs;
+            #refer to an existing derivation
+            inputsFrom = [ bin ];
           };
         }
       );
